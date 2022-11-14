529f878ed93f1a4fd91b56a443101d36dcaaab9b
Aubin <aubin.ollivier@student-cs.fr>
2022-11-14 10:35:59 +0100

    ajout bdd

--- funky_recipe/recipe/models.py
+++ funky_recipe/recipe/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Recette(models.Model):
+    id_rec = models.IntegerField(unique=True)
+    name_rec = models.CharField()
+    link = models.CharField()
+
+class Ingredient(models.Model):
+    id_ing = models.IntegerField(unique=True)
+    name_ing = models.CharField()
+    id_rec = models.ManyToManyField(Recette)
