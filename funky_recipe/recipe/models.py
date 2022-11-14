0695a9c0807006cfa55dfd5d868992081cf88d78
Aubin <aubin.ollivier@student-cs.fr>
2022-11-14 15:08:16 +0100

    nouvelle base de données

--- funky_recipe/recipe/models.py
+++ funky_recipe/recipe/models.py
@@ -3,11 +3,9 @@ from django.db import models
 # Create your models here.
 
 class Ingredient(models.Model):
-    id_ing = models.IntegerField(unique=True)
-    name_ing = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
 class Recette(models.Model):
-    id_rec = models.IntegerField(unique=True)
-    name_rec = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     link = models.CharField(max_length=200)
     id_ing = models.ManyToManyField(Ingredient)
