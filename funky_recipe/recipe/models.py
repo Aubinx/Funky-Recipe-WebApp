0e665d74b414e1104174aa9660125e68620902c8
Aubin <aubin.ollivier@student-cs.fr>
2022-11-14 11:01:45 +0100

    migration bdd

--- funky_recipe/recipe/models.py
+++ funky_recipe/recipe/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class Recette(models.Model):
     id_rec = models.IntegerField(unique=True)
-    name_rec = models.CharField()
-    link = models.CharField()
+    name_rec = models.CharField(max_length=200)
+    link = models.CharField(max_length=200)
 
 class Ingredient(models.Model):
     id_ing = models.IntegerField(unique=True)
-    name_ing = models.CharField()
+    name_ing = models.CharField(max_length=200)
     id_rec = models.ManyToManyField(Recette)
